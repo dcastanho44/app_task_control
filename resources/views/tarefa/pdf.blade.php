ae9dbbae473b30d79df783eaf5c8dc304f503e8c
dcastanho44 <dcastanho44@gmail.com>
2022-03-09 12:42:50 -0300

    243

--- resources/views/tarefa/pdf.blade.php
+++ resources/views/tarefa/pdf.blade.php
@@ -1,1 +1,20 @@
-<h2>Chegamos até aqui<h2>
+<h2>Lista de Tarefas<h2>
+
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Tarefa</th>
+                <th>Data limite de conclusão</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            @foreach($tarefas as $key => $tarefa)
+                <tr>
+                    <td>{{ $tarefa->id }}</td>
+                    <td>{{ $tarefa->tarefa }}</td>
+                    <td>{{ date('d/m/Y', strtotime($tarefa->data_limite_conclusao)) }}</td>
+                </tr>
+            @endforeach
+        </tbody>
