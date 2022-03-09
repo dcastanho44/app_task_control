c11daa6e1f2ffeac970481e9c7a7786c38c0693b
dcastanho44 <dcastanho44@gmail.com>
2022-03-09 09:33:54 -0300

    232

---
+++ resources/views/bem-vindo.blade.php
@@ -0,0 +1,12 @@
+Site da aplicação
+
+@Auth
+    <h1>Usuário autenticado</h1>
+    <p>{{Auth::user()->id}}</p>
+    <p>{{Auth::user()->name}}</p>
+    <p>{{Auth::user()->email}}</p>
+@endauth
+
+@guest
+    Olá visitante
+@endguest
